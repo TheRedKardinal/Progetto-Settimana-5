cdaeeb91e99d28edd92bbec6bee56ae1cd6dfcfc
TheRedKardinal <claudio_giasi@icloud.com>
2026-06-06 01:09:47 +0200

    Header nero quando si scrolla. Commit di fine giornata

--- start/script.js
+++ start/script.js
@@ -2,3 +2,16 @@
 //
 // Aggiungi qui il codice JavaScript se serve (es. modale, scroll handler).
 // Per la Versione Base spesso non serve niente: tutto si fa in CSS.
+
+// ***** SCROLL EVENT *****
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    // Bisogna fare riferimento alla finestra e allo scroll sul suo asse Y (vert)
+    console.log(window.scrollY);
+if (window.scrollY >= 100) {
+    header.classList.add('scrolled')
+} else {
+        header.classList.remove('scrolled');
+}
+});
